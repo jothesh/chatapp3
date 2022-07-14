0d6a73dc9d7c268ca0374b192e6215fc581ad36d
jothesh <jotheshvaran1@gmail.com>
2022-07-14 17:33:13 +0530

    chat

---
+++ index.js
@@ -0,0 +1,105 @@
+const person = prompt("enter your name",'unkown');
+
+const socket = io("ws://192.168.29.137:3000/chat",{query : `name=${person}`});
+// const socket = io("ws://192.168.29.137:3000/chat");
+const username = get("msg-info-name")
+
+const msgerForm = get(".msger-inputarea");
+const msgerInput = get(".msger-input");
+const msgerChat = get(".msger-chat");
+
+// const BOT_MSGS = [
+//   "Hi, how are you?",
+//   "Ohh... I can't understand what you trying to say. Sorry!",
+//   "I like to play games... But I don't know how to play!",
+//   "Sorry if my answers are not relevant. :))",
+//   "I feel sleepy! :("
+// ];
+
+// Icons made by Freepik from www.flaticon.com
+socket.on("user",((name)=>{
+    console.log(name)
+    var user = name;
+    console.log(user,"from uer")
+
+}))
+// console.log(user,"form of")
+const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
+const PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
+const BOT_NAME = "";
+
+const PERSON_NAME = person;
+
+msgerForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const msgText = msgerInput.value;
+  if (!msgText) return;
+  socket.emit("message",msgText);
+
+  appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
+  msgerInput.value = "";
+
+ 
+});
+botResponse();
+
+function appendMessage(name, img, side, text) {
+  //   Simple solution for small apps
+  const msgHTML = `
+    <div class="msg ${side}-msg">
+      <div class="msg-img" style="background-image: url(${img})"></div>
+
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name}</div>
+          <div class="msg-info-time">${formatDate(new Date())}</div>
+        </div>
+
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `;
+
+  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+  msgerChat.scrollTop += 500;
+}
+
+function botResponse() {
+    socket.on("message",((payload) =>
+    {
+        // console.log(payload);
+        // const user=JSON.parse(payload);
+        // console.log(Json.parse(payload));
+        var arr1 = Object.keys(payload);
+var arr2 = Object.values(payload);
+
+console.log(arr2);        
+        appendMessage(arr2[0], BOT_IMG, "left", arr2[1]);
+       
+       
+    }))
+    
+}
+// function userNamesJoined(username){
+//         console.log(`[${username.user}] connected to the chat`)
+       
+//         // alert();
+//     }
+  
+
+// Utils
+function get(selector, root = document) {
+  return root.querySelector(selector);
+}
+
+function formatDate(date) {
+  const h = "0" + date.getHours();
+  const m = "0" + date.getMinutes();
+
+  return `${h.slice(-2)}:${m.slice(-2)}`;
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
